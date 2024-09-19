881f36ef7622f680b4fb453c6d8acd29026524ad
Lucas-G-A <lucas.g.a@icloud.com>
2024-09-18 19:52:08 -0600

    Subir vista de Zama

---
+++ CalculadoraED/src/Prueba.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author edi
+ */
+public class Prueba extends JFrame{
+    
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        NewJFrame ventana = new NewJFrame();
+        ventana.setVisible(true);
+    }
+    
+}
